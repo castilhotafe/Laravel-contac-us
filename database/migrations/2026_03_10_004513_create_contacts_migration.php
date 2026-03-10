1df815f566f6d102fb1841ffcafb01e564f62da7
Konrad Krzyzkowiak <20147441@tafe.wa.edu.au>
2026-03-10 10:24:13 +0800

    wip: add contacts migration

---
+++ database/migrations/2026_03_10_004513_create_contacts_migration.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('given_name', 128)->required;
+            $table->string('middle_name', 128)->nullable();
+            $table->string('family_name', 128)->nullable();
+            $table->string('nickname', 64)->nullable();
+            $table->string('email')->nullable();
+            $table->bigInteger('phone')->nullable();
+            $table->timeStamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            //
+        });
+    }
+};
